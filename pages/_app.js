fdc69a520f3972d97524ecdf710aa945c5fb9c22
Loayei <loayei@gmail.com>
2022-05-10 20:42:58 -0700

    added some styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import Layout from "../components/layout";
+import Layout from "../components/layout/main";
+
+const WEBSITE = ({ Component, pageProps, router }) => {
+    return (
+        <ChakraProvider>
+            <Layout router={router}>
+                <Component {...pageProps} key = {router.route} />
+            </Layout>
+        </ChakraProvider>
+    );
+}
+
+export default WEBSITE;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
     return (
-        <div>Hello World</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I&apos;m a software developer.
+            </Box>
+
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Loai AlFarran
+                    </Heading>
+        <p> Transitioning from Web2 =&gt; Web3</p>
+                </Box>
+            </Box>
+        </Container>
     )
 }
 export default Page
